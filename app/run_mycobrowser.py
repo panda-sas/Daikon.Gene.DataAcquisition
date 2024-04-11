49ae73838dc97825abaf9eaed6ae69229f3fe134
Siddhant Rath <sid@tamu.edu>
2024-04-11 11:24:47 -0500

    folder creation

--- app/run_mycobrowser.py
+++ app/run_mycobrowser.py
@@ -1,19 +1,25 @@
 from etl.sources.source_mycobrowser.data_loader_from_url import read_csv_from_url, read_fasta_from_url
 from etl.sources.source_mycobrowser.data_combiner import combine_data
-from app.utils.file_operations.write_output_mycobrowser import write_output
-
+from utils.file_operations.write_output_mycobrowser import write_output
+import os
 
 def main():
     csv_url = 'https://mycobrowser.epfl.ch/releases/4/get_file?dir=txt&file=Mycobacterium_tuberculosis_H37Rv.txt'
     fasta_gene_url = 'https://mycobrowser.epfl.ch/releases/4/get_file?dir=fasta_genes&file=Mycobacterium_tuberculosis_H37Rv.fasta'
     fasta_protein_url = 'https://mycobrowser.epfl.ch/releases/4/get_file?dir=fasta_proteins&file=Mycobacterium_tuberculosis_H37Rv.fasta'
-    output_file_path = '/Users/saswatipanda/workspace/Daikon.Gene.DataAcquisition/app/data/processed/all_genes_mycobrowser.json'
+    output_file_path = '../data/processed/all_genes_mycobrowser.json'
 
     gene_data = read_csv_from_url(csv_url)
     gene_sequences = read_fasta_from_url(fasta_gene_url)
     protein_sequences = read_fasta_from_url(fasta_protein_url)
 
     combined_output = combine_data(gene_data, gene_sequences, protein_sequences)
+    
+    
+    # Ensure output directory exists
+    output_dir = os.path.dirname(output_file_path)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
     write_output(output_file_path, combined_output)
 
--- app/run_uniprot.py
+++ app/run_uniprot.py
@@ -1,5 +1,5 @@
 # main.py
-
+import os
 from etl.sources.source_uniprot.gene_extractor_uniprot import extract_gene_data
 from etl.sources.source_uniprot.data_fetcher_uniprot import fetch_data
 from utils.file_operations.write_output_uniprot import save_to_json
@@ -7,12 +7,22 @@ from etl.sources.source_uniprot.raw_data_saver import save_raw_data
 
 def main():
     uniprot_url = 'https://rest.uniprot.org/uniprotkb/stream?format=json&query=%28H37Rv%29'
-    raw_data_output_file_path = '/Users/saswatipanda/workspace/Daikon.Gene.DataAcquisition/app/data/raw/raw_uniprot_data.json'
-    processed_data_output_file_path = '/Users/saswatipanda/workspace/Daikon.Gene.DataAcquisition/app/data/processed/all_genes_uniprot.json'
+    raw_data_output_file_path = '../data/raw/raw_uniprot_data.json'
+    processed_data_output_file_path = '../data/processed/all_genes_uniprot.json'
 
 
     try:
         json_data = fetch_data(uniprot_url)
+        
+        # Ensure output directory exists
+        output_dir = os.path.dirname(raw_data_output_file_path)
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        output_dir = os.path.dirname(processed_data_output_file_path)
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+            
+            
 
         if json_data:
             # Save raw data
