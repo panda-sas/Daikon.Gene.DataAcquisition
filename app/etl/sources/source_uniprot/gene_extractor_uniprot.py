193bd9774bfdd20b72b2d8fd569168eedcbac269
Siddhant Rath <sid@tamu.edu>
2024-04-11 12:45:22 -0500

    Proper annotations

--- app/etl/sources/source_uniprot/gene_extractor_uniprot.py
+++ app/etl/sources/source_uniprot/gene_extractor_uniprot.py
@@ -1,52 +1,56 @@
 # Module to extract the genes and proteins properties from Uniprot
 
+
 def extract_gene_data(json_data):
     genes_data = []
-    results = json_data.get('results', [])
+    results = json_data.get("results", [])
 
     for result in results:
-        genes = result.get('genes', [])
+        uniProtId = result.get("primaryAccession", "")
+
+        genes = result.get("genes", [])
         if genes:
             gene_info = genes[0]
-            gene_name = gene_info.get('geneName', {}).get('value', None)
-            ordered_locus_names = gene_info.get('orderedLocusNames', [])
+            gene_name = gene_info.get("geneName", {}).get("value", None)
+            ordered_locus_names = gene_info.get("orderedLocusNames", [])
 
             locus_value = None
             for locus in ordered_locus_names:
-                locus_value = locus.get('value', None)
+                locus_value = locus.get("value", None)
                 if locus_value:
                     break
 
-            protein_description = result.get('proteinDescription', {})
-            recommended_name = protein_description.get('recommendedName', {})
-            full_name = recommended_name.get('fullName', {})
-            protein_name = full_name.get('value', None)
+            protein_description = result.get("proteinDescription", {})
+            recommended_name = protein_description.get("recommendedName", {})
+            full_name = recommended_name.get("fullName", {})
+            protein_name = full_name.get("value", None)
 
             # Check if protein name is not available in recommendedName, try submissionNames
             if protein_name is None:
-                submission_names = protein_description.get('submissionNames', [])
+                submission_names = protein_description.get("submissionNames", [])
                 for submission_name in submission_names:
-                    full_name = submission_name.get('fullName', {})
-                    protein_name = full_name.get('value', None)
+                    full_name = submission_name.get("fullName", {})
+                    protein_name = full_name.get("value", None)
                     if protein_name:
                         break
 
             # Catalytic activity
             catalytic_activities = []
-            comments = result.get('comments', [])
+            comments = result.get("comments", [])
             for comment in comments:
-                comment_type = comment.get('commentType', '').lower()
-                if comment_type == 'catalytic activity':
-                    reaction = comment.get('reaction', {})
-                    activity_name = reaction.get('name', '')
-                    activity_ec_number = reaction.get('ecNumber', '')
-                    publications = reaction.get('evidences', [])
+                comment_type = comment.get("commentType", "").lower()
+                if comment_type == "catalytic activity":
+                    reaction = comment.get("reaction", {})
+                    activity_name = reaction.get("name", "")
+                    activity_ec_number = reaction.get("ecNumber", "")
+                    publications = reaction.get("evidences", [])
                     if activity_name:
-                        catalytic_activities.append({
-                            "Name": activity_name,
-                            "EC number": activity_ec_number,
-                            "Publications": publications
-                        }
+                        catalytic_activities.append(
+                            {
+                                "Name": activity_name,
+                                "EC number": activity_ec_number,
+                                "Publications": publications,
+                            }
                         )
 
             # Gene Ontology
@@ -54,74 +58,98 @@ def extract_gene_data(json_data):
                 "Gene Ontology": {
                     "Cellular Component": [],
                     "Molecular Function": [],
-                    "Biological Process": []
+                    "Biological Process": [],
                 }
             }
 
             # AlphaFold information
-            alpha_fold_info = {
-                "AlphaFoldDB": {
-                    "id": None
-                }
-            }
+            alpha_fold_info = {"AlphaFoldDB": {"id": None}}
 
             # Function information
-            function_info = {
-                "Function": None
-            }
+            function_info = {"Function": []}
 
-            comments = result.get('comments', [])
+            comments = result.get("comments", [])
             for comment in comments:
-                comment_type = comment.get('commentType', '').lower()
-                if comment_type == 'function':
-                    texts = comment.get('texts', [])
+                comment_type = comment.get("commentType", "").lower()
+                if comment_type == "function":
+                    texts = comment.get("texts", [])
                     if texts:
                         for text in texts:
-                            value = text.get('value', '')
-                            function_info["Function"] = value
+                            value = text.get("value", "")
+                            publication = text.get("evidences", [])
+                            function_info["Function"].append(
+                                {"Name": value, "Publications": publication}
+                            )
 
-
-            uniProtKB_cross_references = result.get('uniProtKBCrossReferences', [])
+            uniProtKB_cross_references = result.get("uniProtKBCrossReferences", [])
             for cross_reference in uniProtKB_cross_references:
-                if cross_reference.get('database') == 'GO':  # Check for "database": "GO"
+                if (
+                    cross_reference.get("database") == "GO"
+                ):  # Check for "database": "GO"
                     # go_id = cross_reference.get('id', '')
-                    go_properties = cross_reference.get('properties', [])
+                    go_properties = cross_reference.get("properties", [])
+                    go_evidence = cross_reference.get("evidences", [])
 
                     for prop in go_properties:
-                        if prop.get('key') == 'GoTerm':
-                            go_term_value = prop.get('value', '')
 
+                        if prop.get("key") == "GoTerm":
+                            go_term_value = prop.get("value", "")
 
                             # If value starts with P: then Gene ontology - Biological Process.
                             # If value starts with F: then Gene ontology - Molecular Function.
                             # If value starts with C: then Gene ontology - Cellular Component.
-                            
+
                             # Remove prefixes "C:", "F:", and "P:" from the values
-                            go_term_stripped_value = go_term_value.lstrip("C:").lstrip("F:").lstrip("P:")
+                            go_term_stripped_value = (
+                                go_term_value.lstrip("C:").lstrip("F:").lstrip("P:")
+                            )
 
                             # Determine Gene Ontology category based on properties
                             if go_term_value.startswith("P:"):
-                                gene_ontology["Gene Ontology"]["Biological Process"].append(go_term_stripped_value)
+                                gene_ontology["Gene Ontology"][
+                                    "Biological Process"
+                                ].append(
+                                    {
+                                        "Name": go_term_stripped_value,
+                                        "Evidences": go_evidence,
+                                    }
+                                )
                             elif go_term_value.startswith("F:"):
-                                gene_ontology["Gene Ontology"]["Molecular Function"].append(go_term_stripped_value)
+                                gene_ontology["Gene Ontology"][
+                                    "Molecular Function"
+                                ].append(
+                                    {
+                                        "Name": go_term_stripped_value,
+                                        "Evidences": go_evidence,
+                                    }
+                                )
                             elif go_term_value.startswith("C:"):
-                                gene_ontology["Gene Ontology"]["Cellular Component"].append(go_term_stripped_value)
+                                gene_ontology["Gene Ontology"][
+                                    "Cellular Component"
+                                ].append(
+                                    {
+                                        "Name": go_term_stripped_value,
+                                        "Evidences": go_evidence,
+                                    }
+                                )
 
-                elif cross_reference.get('database') == 'AlphaFoldDB':  # Check for "database": "AlphaFoldDB"
-                    alpha_fold_info["AlphaFoldDB"]["id"] = cross_reference.get('id', None)
+                elif (
+                    cross_reference.get("database") == "AlphaFoldDB"
+                ):  # Check for "database": "AlphaFoldDB"
+                    alpha_fold_info["AlphaFoldDB"]["id"] = cross_reference.get(
+                        "id", None
+                    )
 
             if locus_value or gene_name or protein_name:
                 gene_data = {
-                    "Genes": {
-                        "Locus": locus_value,
-                        "Gene name": gene_name,
-                        "Protein name": protein_name,
-                        "Catalytic Activity": catalytic_activities,
-                        **gene_ontology,
-                        **alpha_fold_info,
-                        **function_info
-                    
-                    }
+                    "UniProtKB": uniProtId,
+                    "Locus": locus_value,
+                    "Gene name": gene_name,
+                    "Protein name": protein_name,
+                    "Catalytic Activity": catalytic_activities,
+                    **gene_ontology,
+                    **alpha_fold_info,
+                    **function_info,
                 }
                 genes_data.append(gene_data)
 
--- app/run_uniprot.py
+++ app/run_uniprot.py
@@ -1,39 +1,56 @@
-# main.py
 import os
+import json
+import logging
+import time
+from datetime import datetime, timedelta
 from etl.sources.source_uniprot.gene_extractor_uniprot import extract_gene_data
 from etl.sources.source_uniprot.data_fetcher_uniprot import fetch_data
 from utils.file_operations.write_output_uniprot import save_to_json
 from etl.sources.source_uniprot.raw_data_saver import save_raw_data
+
+# Setup logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def main():
     uniprot_url = 'https://rest.uniprot.org/uniprotkb/stream?format=json&query=%28H37Rv%29'
     raw_data_output_file_path = '../data/raw/raw_uniprot_data.json'
     processed_data_output_file_path = '../data/processed/all_genes_uniprot.json'
 
-
     try:
-        json_data = fetch_data(uniprot_url)
-        
-        # Ensure output directory exists
-        output_dir = os.path.dirname(raw_data_output_file_path)
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
-        output_dir = os.path.dirname(processed_data_output_file_path)
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
-            
-            
+        logging.info("Ensuring output directories exist.")
+        output_dirs = [os.path.dirname(raw_data_output_file_path), os.path.dirname(processed_data_output_file_path)]
+        for output_dir in output_dirs:
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
+                logging.info(f"Created directory: {output_dir}")
 
-        if json_data:
-            # Save raw data
-            save_raw_data(json_data, raw_data_output_file_path)
+        logging.info("Checking if the raw data file exists and its last modified time.")
+        should_fetch = True
+        if os.path.exists(raw_data_output_file_path):
+            last_modified_time = os.path.getmtime(raw_data_output_file_path)
+            if datetime.fromtimestamp(last_modified_time) > datetime.now() - timedelta(hours=24):
+                should_fetch = False
+                logging.info("Raw data is less than 24 hours old. Skipping fetch.")
 
-            # Extract and save processed data
-            genes_data = extract_gene_data(json_data)
-            save_to_json(genes_data, processed_data_output_file_path)
+        if should_fetch:
+            logging.info("Fetching new data from UniProt.")
+            json_data = fetch_data(uniprot_url)
+            if json_data:
+                logging.info("Saving raw data.")
+                save_raw_data(json_data, raw_data_output_file_path)
+        else:
+            logging.info("Loading existing raw data from file.")
+            with open(raw_data_output_file_path, 'r') as file:
+                json_data = json.load(file)
+
+        logging.info("Extracting gene data.")
+        genes_data = extract_gene_data(json_data)
+
+        logging.info("Saving processed gene data.")
+        save_to_json(genes_data, processed_data_output_file_path)
 
     except Exception as e:
-        print(f"Error: {e}")
+        logging.error(f"Error occurred: {e}")
 
 if __name__ == "__main__":
     main()
